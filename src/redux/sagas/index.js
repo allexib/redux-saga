5d42a1b3f3c4b86e0b03b6a3e2abe476d62ea87b
allexib <ooomegasib@gmail.com>
2022-05-02 16:36:30 +0300

    takeLatest,takeLeading,select

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,13 +1,20 @@
-import {takeEvery} from 'redux-saga/effects'
+import {takeEvery, takeLatest, takeLeading,select} from 'redux-saga/effects'
 import {INCREASE_COUNT, DECREASE_COUNT} from "../constants";
 
+const delay = (time) => new Promise((resolve, reject) => {
+    setTimeout(resolve, time * 1000)
+})
+
 export function* workerSaga() {
-    console.log('request 1')
-    yield
+    const count=yield select(({counter})=>counter.count)
+    yield delay(2)
+    console.log(`request ${count}`)
+
 }
 
 export function* watchClickSaga() {
-    yield takeEvery(INCREASE_COUNT, workerSaga)
+    yield takeLatest(INCREASE_COUNT, workerSaga)
+    yield takeLeading(INCREASE_COUNT, workerSaga)
 
     /*yield take(DECREASE_COUNT)
     console.log('request 2')*/
