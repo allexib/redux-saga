fe30aa898df0aa568582fa2217eb5772fef85671
allexib <ooomegasib@gmail.com>
2022-05-02 15:42:20 +0300

    redux-saga takeEvery

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,4 +1,4 @@
-import {take} from '@redux-saga/core/effects'
+import {takeEvery} from 'redux-saga/effects'
 import {INCREASE_COUNT, DECREASE_COUNT} from "../constants";
 
 export function* workerSaga() {
@@ -6,10 +6,10 @@ export function* workerSaga() {
 }
 
 export function* watchClickSaga() {
-    yield take(INCREASE_COUNT)
-    console.log('request 1')
-    yield take(DECREASE_COUNT)
-    console.log('request 2')
+    yield takeEvery(INCREASE_COUNT, () => console.log('request 1'))
+
+    /*yield take(DECREASE_COUNT)
+    console.log('request 2')*/
 }
 
 export default function* rootSaga() {
